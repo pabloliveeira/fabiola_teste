806f2368e27c7412dafa1ebbca040adf35bfe9aa
Pablo Oliveira <pablo230197@gmail.com>
2023-11-25 22:51:50 -0300

    atualizacao

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-const form = document.getElementById('form-js')
+const form = document.getElementById('form-js');
 
-function compara(n) {
- if document.getElementById('campo-a') <= document.getElementById('campo-b')
-    alert("formulário inválido")
+function compara(campoA, campoB){
+    if (parseFloat(campoB.value) > parseFloat(campoA.value)) {
+        return false;
+    } else {
+        return true;
+    }
 }
-
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 })
+
+let campoA = document.getElementById('campo-a');
+let campoB = document.getElementById('campo-b');
+
+let formvalid = compara(campoA.value, campoB.value);
 
 console.log(form);
