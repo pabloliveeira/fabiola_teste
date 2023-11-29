8ce46647f2911849b11d0b5ecc20250d202e39f5
Pablo Oliveira <pablo230197@gmail.com>
2023-11-28 23:06:42 -0300

    consegui

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 const form = document.getElementById('form-js');
 
+
 function compara(campoA, campoB){
+    const mensagemInvalido = "<b>O FORMULÁRIO É INVÁLIDO, POIS O CAMPO B É MAIOR QUE O CAMPO A</B"
+    const mensagemOk = "<b>O FORMULÁRIO É VÁLIDO</b"
     if (parseFloat(campoB) > parseFloat(campoA)) {
-    return alert("O FORMULÁRIO É INVÁLIDO");
-    } else {
-    return alert("O FORMULÁRIO É VÁLIDO");
+        const containerMensagemInvalido =  document.querySelector('.mensagem');
+         containerMensagemInvalido.innerHTML = mensagemInvalido
+         containerMensagemInvalido.style.display = 'block';
+    
+        } else {
+        const containerMensagemOk = document.querySelector('.mensagem');
+        containerMensagemOk.innerHTML = mensagemOk
+        containerMensagemOk.style.display = 'block';
+        
     }
 }
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    
     let campoA = document.getElementById('campo-a');
     let campoB = document.getElementById('campo-b');
 
-    let formvalid = compara(campoA, campoB);
+    let formvalid = compara(campoA.value, campoB.value);
+    campoA.value = '';
+        campoB.value = '';
 })
 
 console.log(form);
